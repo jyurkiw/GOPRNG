1aeee6631a714af81c6cf5bb29c6b8cfd95c0e12
Jeffrey Yurkiw <jyurkiw@yahoo.com>
2021-07-02 22:14:22 -0700

    Added code comments.

--- prng.go
+++ prng.go
@@ -1,5 +1,8 @@
 package xorshift64
 
+/** Seed a 320-bit xorshift psuedo random number
+generator function that creates int64 values
+and return it. */
 func SeedXOrShift64(a int64, b int64, c int64, d int64, e int64) func() int64 {
 	return func() int64 {
 		x := e
@@ -17,28 +20,45 @@ func SeedXOrShift64(a int64, b int64, c int64, d int64, e int64) func() int64 {
 	}
 }
 
+/** An int64 prng interface containing definitions for
+next, next with a ceiling, and next between a ceiling
+and floor functions.
+*/
 type prng interface {
 	next() int64
 	nextMax(max int64) int64
 	nextBetween(min int64, max int64) int64
 }
 
+/** A basic xorshift struct.
+Contains a generator function.
+*/
 type xorshift struct {
 	generator func() int64
 }
 
+/** Return a psuedo-random int64.
+ */
 func (g xorshift) next() int64 {
 	return g.generator()
 }
 
+/** Return a psuedo-random int64 with a value
+of 0 inclusive < value < max exclusive.
+*/
 func (g xorshift) nextMax(max int64) int64 {
 	return g.generator() % max
 }
 
+/** Return a psuedo-random int64 with a value
+of min inclusive < value < max exclusive.
+*/
 func (g xorshift) nextBetween(min int64, max int64) int64 {
 	return (g.generator() % (max - min)) + min
 }
 
+/** Seed and return an xorshift prng.
+ */
 func new(a int64, b int64, c int64, d int64, e int64) prng {
 	return xorshift{generator: SeedXOrShift64(a, b, c, d, e)}
 }
